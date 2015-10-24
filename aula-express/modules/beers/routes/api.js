4fe3c6a2de510b4100e4a03f52b3fd2404a4261d
Adroaldo Pagliari <adroaldo@outlook.com>
2015-10-24 12:43:55 -0200

    Adicionado api

--- aula-express/modules/beers/routes/api.js
+++ aula-express/modules/beers/routes/api.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var Controller = require('./../controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Listagem de cervejas' });
 });
+
+router.post('/', function(req, res, next){
+	Controller.create(req, res);
+})
 
 module.exports = router;
